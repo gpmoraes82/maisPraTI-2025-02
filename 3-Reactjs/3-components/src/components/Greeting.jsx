5f8f5aa68f4c58183d796fc113f89d66145731a1
Gabriel P Moraes <gpmoraes82@gmail.com>
2025-08-02 09:46:22 -0300

    ajustes em 3-componests component

--- 3-Reactjs/3-components/src/components/Greeting.jsx
+++ 3-Reactjs/3-components/src/components/Greeting.jsx
@@ -3,7 +3,7 @@ function Greeting(props) {
 }
 
 function Aplicativo() {
-    <Greeting name='Jaques'/>
+    return <Greeting name='Jaques'/>
 }
 
 // Criar um componente que exibe as informações de um produto. O componente pai (App) vai passar os dados do produto (nome, preço, descrição) como props para o componente filho (Produto), e o componente filho vai exibir essas informações.
--- 3-Reactjs/3-components/src/main.jsx
+++ 3-Reactjs/3-components/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+import App from './components/App.jsx'
+import Greeting from './components/Greeting.jsx'
+import ComponenteClasse from './components/ComponenteClasse.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Greeting name='Gabriel' />
+    <ComponenteClasse />
   </StrictMode>,
 )
